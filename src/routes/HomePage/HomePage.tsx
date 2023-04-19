8eb5be7e88f31723d3b2382beb763944a414a665
Austin Aitken <54446668+alaitken@users.noreply.github.com>
2023-04-19 21:25:32 -0230

    Update HomePage.tsx
    Updated Key Attributes to Key Acheivements

--- src/routes/HomePage/HomePage.tsx
+++ src/routes/HomePage/HomePage.tsx
@@ -9,16 +9,16 @@ const HomePage = () => {
             <p className={styles.intro}>
                 Welcome to my personal website!
             </p>
-            <p className={styles.intro}>Please make yourself comfortable and get to know me a little...</p>
             <div className={styles.imgContainer}>
                 <img className={styles.img} src={PP}/>
                 <div className={styles.infoContainer}>
-                    <p style={{marginTop: '0px', textDecoration: 'underline', fontSize: '1.4rem'}}>Key Attributes:</p>
+                    <p style={{marginTop: '0px', textDecoration: 'underline', fontSize: '1.4rem'}}>Key Achievements:</p>
                     <ul>
+                        <li className={styles.li}>Won a Hackathon with a Wildlife Trafficking Alert</li>
+                        <li className={styles.li}>Co-Founded a Start-up that reached Tens of Thousands of Users.</li>
+                        <li className={styles.li}>Won the Exemplary Performance Award for All Work Placements.</li>
                         <li className={styles.li}>Experienced Software Developer</li>
-                        <li className={styles.li}>Relentless Work Ethic</li>
-                        <li className={styles.li}>Aspiring Ukulele One-Man-Band</li>
-                        <li className={styles.li}>Seasonal Time-Travler</li>
+                        <li className={styles.li}>Seasonal Time-Travler.</li>
                     </ul>
                 </div>
             </div>
